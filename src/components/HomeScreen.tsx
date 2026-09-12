3adccb44457a58281e46d87d5c21586b22349e1c
marhabanid91-debug <marhabanid91@gmail.com>
2026-09-13 00:42:48 +0300

    Update HomeScreen.tsx

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { useApp } from "../context/AppContext";
-import marhabaLogo from "../imports/2177178-removebg-preview__1_.png";
+import marhabaLogo from "../imports/marhaba-logo.png";
 import { ActiveMode, Identity } from "../types";
 import ShareModal from "./ShareModal";
 import { renameIdentity } from "../lib/db";
